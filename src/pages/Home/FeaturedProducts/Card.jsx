2204fb50ccba80bf1214e957ec5b7ae364eebdfe
MoRsHeD <mdmorshed0187@gmail.com>
2024-04-17 00:14:17 +0600

    updated responsive issues

--- src/pages/Home/FeaturedProducts/Card.jsx
+++ src/pages/Home/FeaturedProducts/Card.jsx
@@ -5,7 +5,7 @@ import { FaDollarSign } from "react-icons/fa6";
 
 const Card = ({img}) => {
     return (
-        <div className='mb-5 mt-4 mx-2.5 '>
+        <div className='mb-5 mt-4 mx-0'>
             <div className="">
                 <div className='lg:h-[450px] md:h-[380px] h-[300px] '>
                     <img alt="Product Image" className="w-full h-full object-cover " src={img} />
@@ -13,7 +13,6 @@ const Card = ({img}) => {
                 <div className='mt-3 flex flex-col gap-1'>
                     <h1 className="font-medium">Stylish Black Suit</h1>
                     <h3 className='font-bold'>BDT 5000</h3>
-
                 </div>
             </div>
         </div>
